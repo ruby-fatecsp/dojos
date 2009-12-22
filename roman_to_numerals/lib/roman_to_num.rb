14e7c02ad48667e051dd706d8b567b337199db43
Rafael Rosa <rafael.rosa@locaweb.com.br>
2009-12-22 16:22:43 -0200

    Pequena refatoração

--- roman_to_numerals/lib/roman_to_num.rb
+++ roman_to_numerals/lib/roman_to_num.rb
@@ -1,28 +1,24 @@
 class RomanToNum
+  ROMAN = { 
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000
+          }
+
   def convert(roman)
     roman = roman.chars.to_a
-    if roman[0].nil?
-      0
-    elsif roman.size == 1
-      to_num(roman.shift)
-    elsif to_num(roman[0]) < to_num(roman[1])
-      -to_num(roman.shift) + convert(roman.join)
+
+    case
+      when roman.empty?    : 0
+      when roman.size == 1 : ROMAN[roman.shift]
+      when ROMAN[roman[0]] < ROMAN[roman[1]]
+        -ROMAN[roman.shift] + convert(roman.join)
     else
-      to_num(roman.shift) + convert(roman.join)
+      ROMAN[roman.shift] + convert(roman.join)
     end
   end
-
-  private
-  def to_num(roman)
-    @hash ||= { 'I' => 1,
-      'V' => 5,
-      'X' => 10,
-      'L' => 50,
-      'C' => 100,
-      'D' => 500,
-      'M' => 1000
-    }
-    @hash[roman]
-  end
 end
-
