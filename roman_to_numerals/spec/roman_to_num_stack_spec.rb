96f91df732669cc781948e6ec97be9b8afdb8558
Rafael Rosa <rafael.rosa@locaweb.com.br>
2009-12-22 17:32:28 -0200

    Refatorados os testes para serem utilizados por todas as classes

--- roman_to_numerals/spec/roman_to_num_stack_spec.rb
+++ roman_to_numerals/spec/roman_to_num_stack_spec.rb
@@ -1,55 +1,6 @@
+require File.dirname(__FILE__) + '/spec_helper'
 require 'lib/roman_to_num_stack'
+
 describe RomanToNumStack do
-
-  it 'should convert a empty String to 0' do
-    RomanToNumStack.new.convert('').should == 0
-  end
-
-  it 'should convert I to 1' do
-    RomanToNumStack.new.convert('I').should == 1
-  end
-
-  it 'should convert V to 5' do
-    RomanToNumStack.new.convert('V').should == 5
-  end
-
-  it 'should convert X to 10' do
-    RomanToNumStack.new.convert('X').should == 10
-  end
-
-  it 'should convert L to 50' do
-    RomanToNumStack.new.convert('L').should == 50
-  end
-
-  it 'should convert C to 100' do
-    RomanToNumStack.new.convert('C').should == 100
-  end
-
-  it 'should convert D to 500' do
-    RomanToNumStack.new.convert('D').should == 500
-  end
-
-  it 'should convert M to 1000' do
-    RomanToNumStack.new.convert('M').should == 1000
-  end
-
-  it 'should convert II to 2' do
-    RomanToNumStack.new.convert('II').should == 2
-  end
-
-  it 'should convert VI to 6' do
-    RomanToNumStack.new.convert('VI').should == 6
-  end
-
-  it 'should convert IV to 4' do
-    RomanToNumStack.new.convert('IV').should == 4
-  end
-
-  it 'should convert XCIV to 94' do
-    RomanToNumStack.new.convert('XCIV').should == 94
-  end
-
-  it 'should convert MCMXCVIII to 1998' do
-    RomanToNumStack.new.convert('MCMXCVIII').should == 1998
-  end
+  it_should_behave_like 'roman numerals converters'
 end
---
+++ roman_to_numerals/spec/spec_helper.rb
@@ -0,0 +1,1 @@
+require File.dirname(__FILE__) + '/shared_specs'
