e36c8280958d15ef627d90f90487f05d5a2fe534
Tomas D'Stefano <tomasdestefi@gmail.com>
2010-02-27 21:43:32 -0300

    Adicionado uma excessao p/ as pessoas que nao possuem o rspec-i18n instalado

--- boliche/spec_helper.rb
+++ boliche/spec_helper.rb
@@ -1,6 +1,13 @@
 require 'rubygems'
-require 'spec-i18n'
 require 'boliche'
+
+begin
+  require 'spec-i18n'
+rescue LoadError
+  puts("Voce nao tem instalado a gem rspec-i18n")
+  puts("Para Instalar rode o comando: [sudo] gem install rspec-i18n")
+end
+
 
 Spec::Runner.configure do |config|
   config.spec_language :pt
